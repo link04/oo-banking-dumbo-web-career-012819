985973ca065ad69a637eca0d20dd64d1af108559
Maximo Bautista <max.bautista97@gmail.com>
2019-02-03 19:38:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
     
   attr_reader :sender , :receiver , :amount
-  attr_reader :status
+  attr_accessor :status
   
   def initialize(sender, receiver, amount)
     @sender = sender
