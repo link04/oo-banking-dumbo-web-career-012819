9f7ab6e307429b1b3f5095005ad6ad534ece0f37
Maximo Bautista <max.bautista97@gmail.com>
2019-02-03 19:30:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     if sender.valid? && sender.balance >= @amount
       sender.balance -= @amount
       receiver.balance += @amount
-      
+      @status = 'complete'
     end
   end
   
