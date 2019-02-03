0f7899c54721c79bcd16a0cb2491e5e54d7643ae
Maximo Bautista <max.bautista97@gmail.com>
2019-02-03 20:04:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,11 @@ class Transfer
   
   def execute_transaction
     if sender.valid? && @status == 'pending' && sender.balance >= @amount
-      if 
+     
         sender.balance -= @amount
         receiver.balance += @amount
          @status = 'complete'
-      end
+    
     else
       @status = 'rejected'
       "Transaction rejected. Please check your account balance."
