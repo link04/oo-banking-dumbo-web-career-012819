4a7929b63d79284014a3ce1ec63bdc1755f31e46
Maximo Bautista <max.bautista97@gmail.com>
2019-02-03 19:50:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,6 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       @status = 'complete'
-    elsif sender.valid? == false
       "Transaction rejected. Please check your account balance."
     end
    # binding.pry
